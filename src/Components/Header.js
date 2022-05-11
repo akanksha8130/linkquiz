5b68d2a06a6bbdf4a71bac3f61da6d4d7d963d24
akanksha8130 <71652318+akanksha8130@users.noreply.github.com>
2022-05-11 16:47:58 +0530

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+
+
+
+
+import React from "react";
+ 
+  // header Component
+
+const Header= () => {
+  return(
+    <header className="Header"> 
+      <h1>   Quiz App </h1>
+    </header>
+  );
+}
+
+export default Header;
+
+
+
